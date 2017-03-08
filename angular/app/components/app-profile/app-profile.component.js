a8b2ab20745445e71fb34e07d6fd23973d142ded
metszz <emeritob@zylun.com>
2017-03-09 05:57:25 +0800

    Add profile page.

---
+++ angular/app/components/app-profile/app-profile.component.js
@@ -0,0 +1,17 @@
+class AppProfileController{
+    constructor(){
+        'ngInject';
+
+        //
+    }
+
+    $onInit(){
+    }
+}
+
+export const AppProfileComponent = {
+    templateUrl: './views/app/components/app-profile/app-profile.component.html',
+    controller: AppProfileController,
+    controllerAs: 'profileCtrl',
+    bindings: {}
+}
